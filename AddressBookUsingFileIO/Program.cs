9cc826b6a76e8a9b229a56443e276cb3dddea98a
Subhash Barela <barelasubhash16@gmail.com>
2023-07-16 13:31:00 +0530

    [subhash] write person contact to file

--- AddressBookUsingFileIO/Program.cs
+++ AddressBookUsingFileIO/Program.cs
@@ -10,7 +10,16 @@ namespace AddressBookUsingFileIO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Shiva", "Barela", "GERMANY", "7869054332", "shiva37@gmail.com");
+            Contacts newContact2 = new Contacts("vinnu", "Pawra", "INDIA", "988765465", "Vinnu243@gmail.com");
+            myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact2);
+            myAddressBook.PrintContacts();
+            string path = @"C:\Users\Shiva027\Desktop\NewRemapSession\AddressBookUsingFileIO\AddressBookUsingFileIO\Example.txt";
+
+            myAddressBook.WriteToFile(path);
+            //myAddressBook.ReadFromFile(path);
         }
     }
 }
